3fbcab488356a34e135bb55707eed1d066331283
Recursivex5137 <recursivex5137@gmail.com>
2018-02-08 19:43:41 -0500

    Add react-router package and demonstrate basic router functionality.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
+
 import logo from './logo.svg';
 import './App.css';
+
+import HomePage from './components/home-page';
 
 class App extends Component {
   render() {
@@ -11,8 +14,8 @@ class App extends Component {
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <HomePage></HomePage>
       </div>
     );
   }
